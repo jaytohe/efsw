cedaaf5b584705ef4ad015c8809fc4e5ef0d396b
jaytohe <jaytohe@github.com>
2024-09-23 18:49:43 +0300

    added support for more methods and watcher options

--- src/pyefsw.cpp
+++ src/pyefsw.cpp
@@ -31,14 +31,21 @@ public:
 
 PYBIND11_MODULE(pyefsw, m) {
 	py::class_<efsw::FileWatcher>(m, "FileWatcher")
-		.def(py::init<bool>()) // Only allow initialization with explicit constructor FileWatcher( bool useGenericFileWatcher );
+		.def(py::init<>())
+		.def(py::init<bool>(), py::arg("useGenericFileWatcher")) // Only allow initialization with explicit constructor FileWatcher( bool useGenericFileWatcher );
+		.def("addWatch", static_cast<efsw::WatchID (efsw::FileWatcher::*)(const std::string&, efsw::FileWatchListener*)>(&efsw::FileWatcher::addWatch))
 		.def("addWatch", static_cast<efsw::WatchID (efsw::FileWatcher::*)(const std::string&, efsw::FileWatchListener*, bool)>(&efsw::FileWatcher::addWatch))
+		.def("addWatch", static_cast<efsw::WatchID (efsw::FileWatcher::*)(const std::string&, efsw::FileWatchListener*, bool, const std::vector<efsw::WatcherOption>&)>(&efsw::FileWatcher::addWatch))
 		.def("watch", &efsw::FileWatcher::watch)
 		.def("removeWatch", static_cast<void (efsw::FileWatcher::*)(const std::string&)>(&efsw::FileWatcher::removeWatch))
 		.def("removeWatch", static_cast<void (efsw::FileWatcher::*)(efsw::WatchID)>(&efsw::FileWatcher::removeWatch))
-		.def("directories", &efsw::FileWatcher::directories);
+		.def("directories", &efsw::FileWatcher::directories)
+		.def("followSymlinks", static_cast<void (efsw::FileWatcher::*)(bool)>(&efsw::FileWatcher::followSymlinks))
+		.def("isFollowingSymlinks", static_cast<const bool& (efsw::FileWatcher::*)() const>(&efsw::FileWatcher::followSymlinks))
+		.def("allowOutOfScopeLinks", static_cast<void (efsw::FileWatcher::*)(bool)>(&efsw::FileWatcher::allowOutOfScopeLinks))
+       .def("isAllowingOutOfScopeLinks", static_cast<const bool& (efsw::FileWatcher::*)() const>(&efsw::FileWatcher::allowOutOfScopeLinks));
 
-       // Specify trampoline class for FileWatchListener which handles control back to Python (so we can implement the FileWatchListener interface on the Python side)
+	// Specify trampoline class for FileWatchListener which handles control back to Python (so we can implement the FileWatchListener interface on the Python side)
 	py::class_<efsw::FileWatchListener, PyFileWatchListener>(m, "FileWatchListener")
 		.def(py::init<>())
 		.def("handleFileAction", &efsw::FileWatchListener::handleFileAction);
@@ -49,6 +56,17 @@ PYBIND11_MODULE(pyefsw, m) {
 		.value("Delete", efsw::Action::Delete)
 		.value("Modified", efsw::Action::Modified)
 		.value("Moved", efsw::Action::Moved)
+		.export_values();
+
+	py::class_<efsw::WatcherOption>(m, "WatcherOption")
+		.def(py::init<efsw::Option, int>(), py::arg("option"), py::arg("value"))
+		.def_readwrite("option", &efsw::WatcherOption::mOption)
+		.def_readwrite("value", &efsw::WatcherOption::mValue);
+
+	// Make the Option enum available to Python side
+	py::enum_<efsw::Option>(m, "Option")
+		.value("WinBufferSize", efsw::Option::WinBufferSize)
+		.value("WinNotifyFilter", efsw::Option::WinNotifyFilter)
 		.export_values();
 
 }
